4871e6a84b6c961c42250ec3a56fe5e61dc06272
phhu <phhu.git@gmail.com>
2018-11-17 10:49:08 +0000

    getting folders using superagent

--- src/filesFromFolder.js
+++ src/filesFromFolder.js
@@ -1,6 +1,6 @@
-const {partition,filter,pipe,pluck,map,prop,zipObj,unnest} = require('ramda');
-const scrapeIt = require("scrape-it");
+const {pipe,map,zipObj,unnest} = require('ramda');
 const parseUrl = require('url-parse');
+const request = require('superagent');
 
 // recurse folders getting files
 const getFilesInTree = folder => {
@@ -9,50 +9,52 @@ const getFilesInTree = folder => {
   return fc ? ff.concat(unnest(map(getFilesInTree,fc))) : ff;
 }
 
+const folderRe = /(?<=href=")[^".]+(?=")/gi;        //folders don't have a dot
+const fileRe = /(?<=href=")[^".]+\.[^"]*(?=")/gi;   //files have a dot in them
+
+const getFullUrl = (f,origin) => parseUrl(f,origin).href;
+
 const getFolderContents = url => {
-  console.log("getFolderContents",url);
-  return scrapeIt(url, {
-    links: {
-      listItem: 'a'    // could add [class], but easier to filter later by url length
-      ,data: {
-        href: {attr: "href"}
-        ,size: 'span.size'
-      }
-    }
+  //console.log("getFolderContents",url);
+  const urlParsed =  parseUrl(url);
+  return request.get(url)
+  .then(res => {
+    const html = res.text || '' ;
+    const folders = (html.match(folderRe) || [])
+      .filter(f=>f.length > urlParsed.pathname.length);
+    const files = html.match(fileRe) || [];
+    return [files,folders];
   })
-  .then(async ({ data, response }) => {
-    console.log(url, `Status Code: ${response.statusCode}`)
-    const urlParsed =  parseUrl(url);
+  .then(async (data) => {
     const ret = pipe(
-      prop('links')
-      ,partition(f=>f.size)     //files have sizes, folders don't 
-      ,map(pipe(
-        filter(f=>f.href.length > urlParsed.pathname.length)    // subfolders and files must have longer paths
-        ,pluck('href')
-        ,map(href=>parseUrl(href,urlParsed.origin).href)     // add the base of the original url
-      ))
+      map(map(href=>getFullUrl(href,urlParsed.origin)))     // add the base of the original url
       ,zipObj(['files','folders'])      // make object
     )(data);
     // recurse folder structure
     ret.folderContents = await Promise.all(ret.folders.map(f=>getFolderContents(f)));
     return ret;
   })
+  .catch(err => console.error("Error in getFolderContents", err))
 };
-
-const logJson = label => x=>console.log(label,JSON.stringify(x,null,2));
 
 export const getFilesUnderFolder = url => 
   getFolderContents(url)
   .then(getFilesInTree)
-
+  //.then(logJson("files"))
+;
 /*module.exports = {
   getFilesUnderFolder
 };*/
 
+/*const logJson = label => x=>{
+  console.log(label,JSON.stringify(x,null,2));
+  return x;
+}*/
+
 //getFolderContents('http://localhost:8081/logs/Integration%20Server/Plugin_RIExtender/2018-10-17').then(logJson('f1'));
 /*
 const test = () =>
-  getFolderContents('http://localhost:8081/logs/Integration%20Server/')
+  getFolderContents('http://localhost:8081/logs/Integration%20Server/Plugin_RIExtender/')
   .then(getFilesInTree)
   .then(logJson('f2'));
 
@@ -75,3 +77,19 @@ const testData = {
 logJson("merged")(mergeFolders(testData));
 console.log();
 */
+
+  /*return scrapeIt(url, {
+    links: {
+      listItem: 'a'    // could add [class], but easier to filter later by url length
+      ,data: {
+        href: {attr: "href"}
+        ,size: 'span.size'
+      }
+    }
+  })*/
+
+
+        //prop('links')
+      //,partition(f=>f.size)     //files have sizes, folders don't 
+      //filter(f=>f.href.length > urlParsed.pathname.length)    // subfolders and files must have longer paths
+      //,pluck('href')
